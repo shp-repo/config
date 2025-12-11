9539816add1cb3ce07724cdb4dca63b4a2b31cb3
Sang Hyun Park <shp.repo@gmail.com>
2025-12-11 17:29:21 +0900

    disable treesitter

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,3 +1,45 @@
+--[[
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    -- Temporary setup for lsp & treesitter working together
+    -- Hightlighting
+    -- vim.treesitter.start()
+    -- Folds
+    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    -- vim.wo.foldmethod = 'expr'
+
+    -- https://lsp-zero.netlify.app/blog/lsp-client-features.html
+    -- Custom keymaps
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', '<C-Space>', '<C-x><C-o>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+
+    vim.keymap.set('n', 'grt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'grd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+
+    -- Autotrigger: show the completion menu
+    --              when type a "trigger char" set by the language server
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+    end
+
+    -- Format on save
+    -- Inlay hints
+    -- Highlight the references of the symbol under the cursor
+    -- Simple tab completion
+
+  end,
+})
+--]]
+
+
+-- require'nvim-treesitter'.install { 'bash', 'lua', 'markdown', 'c', 'julia' }
+
+
 require'cmp'.setup {
   sources = {
     { name = 'nvim_lsp' }
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+  -- Treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
+    build = ':TSUpdate'
+  },
+
+  -- Completion
+  { 'hrsh7th/nvim-cmp' },
+  { 'hrsh7th/cmp-nvim-lsp' },
+}
