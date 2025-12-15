9b338f859ce8b2b89fb885c33bc837c577e0927d
Sang Hyun Park <shp.repo@gmail.com>
2025-12-15 14:53:47 +0900

    change font in wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,7 +12,7 @@ config.initial_cols = 96
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font = wezterm.font 'CaskaydiaMono Nerd Font Mono'
+config.font = wezterm.font 'CaskaydiaCove Nerd Font'
 config.font_size = 14
 config.line_height = 1.1
 
@@ -44,7 +44,7 @@ config.window_frame = {
   -- Whatever font is selected here, it will have the
   -- main font setting appended to it to pick up any
   -- fallback fonts you may have used there.
-  font = wezterm.font { family = 'CaskaydiaMono Nerd Font Mono', weight = 'Bold' },
+  font = wezterm.font { family = 'CaskaydiaCove Nerd Font', weight = 'Bold' },
 
   -- The size of the font in the tab bar.
   -- Default to 10.0 on Windows but 12.0 on other systems
